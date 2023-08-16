553c0aeedd69c2f4ff9310b3fe7606520987da85
Mingyang-Li <mingyangli118@gmail.com>
2023-08-16 12:41:58 +1200

    ☀️ init type-safe sdk

---
+++ src/clickup.service.ts
@@ -0,0 +1,9 @@
+/**
+ * @description Adds 2 numbers
+ * @param a number
+ * @param b number
+ * @returns number
+ */
+export const addInt = (a: number, b: number) => a + b;
+
+export const minusInt = (a: number, b: number) => a - b;
