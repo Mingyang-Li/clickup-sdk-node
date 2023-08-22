8bf71ca2afb98acd392a3f54552ef5317205babf
Mingyang-Li <mingyangli118@gmail.com>
2023-08-22 15:40:33 +1200

    test: remove redundant or clause for api key in config

--- tests/authorization.service.spec.ts
+++ tests/authorization.service.spec.ts
@@ -5,7 +5,7 @@ describe('AuthorizationService', () => {
   describe('AuthorizationService.getAuthorizedUser', () => {
     const args: AuthorizationService.GetAuthorizedUserArgs = {
       config: {
-        apiKey: env.CLICKUP_API_KEY || '',
+        apiKey: env.CLICKUP_API_KEY,
       },
     };
     it(`Should return ok`, async () => {
@@ -27,7 +27,7 @@ describe('AuthorizationService', () => {
   describe('AuthorizationService.getAuthorizedTeams', () => {
     const args: AuthorizationService.GetAuthorizedTeamsArgs = {
       config: {
-        apiKey: env.CLICKUP_API_KEY || '',
+        apiKey: env.CLICKUP_API_KEY,
       },
     };
     it(`Should return ok`, async () => {
